a8d7469922b0509ca3bd23879e6015ee8c489768
AAKvashnin <alexey.kvashnin@gmail.com>
2024-10-05 21:12:50 +0300

    init commit

--- build.sbt
+++ build.sbt
@@ -1,1 +1,8 @@
 scalaVersion := "2.13.12"
+
+libraryDependencies ++= Dependencies.zio
+libraryDependencies ++= Dependencies.zioConfig
+libraryDependencies += Dependencies.zioHttp
+libraryDependencies += Dependencies.liquibase
+libraryDependencies += Dependencies.postgres
+libraryDependencies ++= Dependencies.quill
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,33 @@ import sbt.Keys.libraryDependencies
 import sbt._
 
 object Dependencies {
+  lazy val ZioVersion = "1.0.4"
+  lazy val ZIOHttpVersion = "1.0.0.0-RC27"
+  lazy val LiquibaseVersion = "3.4.2"
+  lazy val PostgresVersion = "42.3.1"
+
+  lazy val zio: Seq[ModuleID] = Seq(
+    "dev.zio" %% "zio" % ZioVersion,
+    "dev.zio" %% "zio-test" % ZioVersion,
+    "dev.zio" %% "zio-test-sbt" % ZioVersion,
+    "dev.zio" %% "zio-macros" % ZioVersion
+  )
+
+  lazy val zioConfig: Seq[ModuleID] = Seq(
+    "dev.zio" %% "zio-config" % "1.0.5",
+    "dev.zio" %% "zio-config-magnolia" % "1.0.5",
+    "dev.zio" %% "zio-config-typesafe" % "1.0.5"
+  )
+
+  lazy val zioHttp = "io.d11" %% "zhttp" % ZIOHttpVersion
+
+  lazy val quill = Seq(
+    "io.getquill" %% "quill-jdbc-zio" % "3.12.0",
+    "io.github.kitlangton" %% "zio-magic" % "0.3.11"
+  )
+
+  lazy val liquibase = "org.liquibase" % "liquibase-core" % LiquibaseVersion
+
+  lazy val postgres = "org.postgresql" % "postgresql" % PostgresVersion
 
 }
