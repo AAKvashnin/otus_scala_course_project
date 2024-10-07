5529799ba81a3ea945bd74d269285badf09bcc2e
AAKvashnin <alexey.kvashnin@gmail.com>
2024-10-07 22:59:19 +0300

    init commit

---
+++ src/main/scala/ru/otus/icebergcatalog/db/package.scala
@@ -0,0 +1,7 @@
+package ru.otus.icebergcatalog
+
+package object db {
+
+
+
+}
--- src/main/scala/ru/otus/icebergcatalog/services/IcebergCatalogService.scala
+++ src/main/scala/ru/otus/icebergcatalog/services/IcebergCatalogService.scala
@@ -1,7 +1,20 @@
 package ru.otus.icebergcatalog.services
+
+import zio.{Has, RIO, ZIO, ZLayer}
+import zio.macros.accessible
+import zio.random.Random
+
+
 @accessible
 object IcebergCatalogService {
 
-  
+  type IcebergCatalogService = Has[Service]
+
+  trait Service {
+
+
+  }
+
+  class Impl
 
 }
